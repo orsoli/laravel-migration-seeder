ff2fda06596b092ebc4587cf0cced9938fe9d23f
orsoli <fljcoli@gmail.com>
2024-10-25 15:04:07 +0200

    Implemented create table function in migration and migrate it

--- database/migrations/2024_10_25_123611_create_trains_table.php
+++ database/migrations/2024_10_25_123611_create_trains_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('company', 255);
+            $table->string('departure_station',255);
+            $table->string('arrival_station',255);
+            $table->time('departure_time');
+            $table->time('arrival_time');
+            $table->integer('train_number')->unsigned();
+            $table->smallInteger('number_of_carriages')->unsigned();
+            $table->boolean('on_time', false);
+            $table->boolean('cancelled', false);
             $table->timestamps();
         });
     }
