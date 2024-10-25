d75fba3ab12f1ef242d0345571545281609437fe
orsoli <fljcoli@gmail.com>
2024-10-25 14:19:10 +0200

    Create Controller and Model. Update web.php page implemented route for traincontroller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\TrainController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('pages.home');
-});
+Route::get('/', [TrainController::class, 'home'])->name('home');
