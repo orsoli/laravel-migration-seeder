4dee852b9c9506724edba94f75e0e74309ece6ee
orsoli <fljcoli@gmail.com>
2024-10-28 12:10:03 +0100

    created table

--- database/migrations/2024_10_28_110416_create_passengers_table.php
+++ database/migrations/2024_10_28_110416_create_passengers_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('passengers', function (Blueprint $table) {
             $table->id();
+            $table->string('firs_name');
+            $table->string('last_name');
+            $table->tinyInteger('age')->unsigned();
             $table->timestamps();
         });
     }
