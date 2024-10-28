5d4115387e5c39c8bcbc7be36637fb8c95603266
orsoli <fljcoli@gmail.com>
2024-10-28 17:58:36 +0100

    Implemented function to insert faker data in trains table in db

--- database/seeders/TrainSeeder.php
+++ database/seeders/TrainSeeder.php
@@ -2,16 +2,30 @@
 
 namespace Database\Seeders;
 
+use App\Models\Train;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class TrainSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+        for($i = 0; $i < 20; $i++){
+            $newTrain = new Train();
+            $newTrain->company = $faker->word();
+            $newTrain->departure_station = $faker->city();
+            $newTrain->arrival_station = $faker->city();
+            $newTrain->departure_time = $faker->time();
+            $newTrain->arrival_time = $faker->time();
+            $newTrain->train_number = $faker->numberBetween(1000, 10000);
+            $newTrain->number_of_carriages = $faker->numberBetween(1,20);
+            $newTrain->on_time = $faker->boolean();
+            $newTrain->cancelled = $faker->boolean();
+            $newTrain -> save();
+        }
     }
 }
